bfd76c384ccb781c2061fd54f05e4f00945ff778
AllanLobato <allandonizetti@gmail.com>
2022-10-31 09:02:10 -0300

    FIrst commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,149 @@
+import { HandPalm, Play } from 'phosphor-react'
+import { useForm, FormProvider } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { createContext, useState } from 'react'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdown'
+
+interface Cycle {
+  // os ciclos devem ser identificados por um id
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date // tanto data quanto horario
+  interrupteDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesContextType {
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  amountSecondsPassed: number
+  markCurrentAsFinished: () => void // void quer dizer que não tem retorno
+  setSecondsPassed: (seconds: number) => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextType)
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(1, 'O ciclo precisa ser de no mínimo 1 minuto')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+// controled / uncontrolled - Os formuladrios podem ser uma dessas opções. COntrolled ganha na fluidez mas perde em relação a performande em aplicações grandes.
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> // Sempre que for referenciar uma variável javascript dentro do Typescript é necessário utilizar o type of, caso contrario dá erro.
+
+export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]) // Sempre iniciar o estado com o tipo de informação que vou manusear a aplicação toda
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      // Define um valor inicial
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function markCurrentAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, finishedDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+  }
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    }
+
+    setCycles((state) => [...state, newCycle]) // Sempre que uma alteração de estado depender do valor anterior, usar arrow function
+    setActiveCycleId(id)
+    setAmountSecondsPassed(0)
+
+    reset() // reseta para os valores originais definidos lá no default values
+  }
+
+  function handleInterruptCycle() {
+    // Se o ciclo for o ciclo ativo, eu vou retornar todos os dados do ciclo, porém vou add uma nova informação chamada interruptedDate como sendo a data atual. Seão, retorno o ciclo sem alterações
+
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id === activeCycleId) {
+          return { ...cycle, interrupteDate: new Date() }
+        } else {
+          return cycle
+        }
+      }),
+    )
+    setActiveCycleId(null)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <CyclesContext.Provider
+          value={{
+            activeCycle,
+            activeCycleId,
+            markCurrentAsFinished,
+            amountSecondsPassed,
+            setSecondsPassed,
+          }}
+        >
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
+          <Countdown />
+        </CyclesContext.Provider>
+
+        {activeCycle ? (
+          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+            <HandPalm size={24} />
+            Parar
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
